60c2d951cc0cf2bae3e4ade9ea96afe137b4e1c9
Dmitry Kirilyuk <gk.joker@gmail.com>
2016-03-29 09:48:11 +0300

    Export formatters

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const validate     = require('./lib/validate');
-const TimeoutError = require('./lib/errors/timeoutError');
-const validators   = require('./lib/validators');
+/**
+ * todo:
+ * 1) переименовать config в schema
+ */
+const validate   = require('./lib/validate');
+const validators = require('./lib/validators');
 
-validate.errors = {
-    TimeoutError: TimeoutError
-};
+validate.formatters = require('./lib/formatters');
+validate.errors     = require('./lib/errors');
 
 Object.defineProperty(validate, 'validators', {
-    get: function () {
+    get: function() {
         return validators.getAll();
     },
     set: function($validators) {
